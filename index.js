884a3332116a430a20412d35a52534d0dd8b15d7
fiction <footedroom575@gmail.com>
2022-08-09 00:28:31 +0500

    working code wih basic required features

--- index.js
+++ index.js
@@ -2,20 +2,35 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+let PORT = process.env.PORT || 3000
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+
+// need to use the middleware before accessing the User Agent of a request
+app.use(function(req, res, next) {
+  res.locals.ua = req.get('User-Agent');
+  next();
+});
+
+app.get('/api/whoami', function (req, res) {
+  let resObj = {
+    // information on why getting ip from this method is better
+    // https://stackoverflow.com/a/10849772/13525159
+    ipaddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+
+    language: req.headers['accept-language'],
+    software: req.headers["user-agent"]
+  }
+  console.log(resObj)
+  res.json(resObj);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
